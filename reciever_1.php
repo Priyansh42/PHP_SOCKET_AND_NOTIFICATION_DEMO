4b8d5de6047a8edc8c38ff06e10c3e591a5e2858
Priyansh Shah <68781375+Priyansh42@users.noreply.github.com>
2023-01-17 15:26:08 +0530

    Receive Data in UI along with notification

--- reciever_1.php
+++ reciever_1.php
@@ -4,35 +4,48 @@
     <meta charset="UTF-8">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.2.3/css/bootstrap.min.css"/>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.3/jquery.min.js"></script>
-    <!-- <script src="https://cdnjs.cloudflare.com/ajax/libs/socket.io/2.5.0/socket.io.min.js"></script> -->
-    <title>Title</title>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/socket.io/2.5.0/socket.io.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/2.11.6/umd/popper.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/notify/0.4.2/notify.js"></script>
+    <title>Reciever</title>
 </head>
 <body>
-    <script src='//cdnjs.cloudflare.com/ajax/libs/socket.io/2.5.0/socket.io.min.js'></script>
+    <table class="table">
+        <tr>
+            <th>First Name</th>
+            <th>Last Name</th>
+        </tr>
+    </table>
 
-    <script>
-    var socket = io.connect('http://localhost:3001');
+    <script type="text/javascript">
+        var socket = io.connect("http://localhost:3001", {reconnect: true});
 
-    // Here this function used for connect client with server
-    // You missed this function
-    socket.on('connect', function () {
-        console.log('connected');
+        socket.on('connect', function () {
+            console.log('Server Connected...');
 
-        if(socket.connected != false)
-        {
-            socket.on("new_order",function (data){
-                console.log(data);
-            })
-        }
-        else
-        {
-            console.log("Cannot Connect!");
-        }
+            if(socket.connected != false)
+            {
+                socket.on("new_order",function (data){
+                    $("table").append("<tr><td>"+data.first_name+"</td><td>"+data.last_name+"</td></tr>"); // replaceWith to replace
+                    $.notify("First Name: "+data.first_name+"\nLast Name: "+data.last_name, {
+                        autoHide: false,
+                        className: "info",
+                        showAnimation: 'slideDown',
+                        hideAnimation: 'slideUp',
+                        elementPosition: 'bottom right',
+                        globalPosition: 'bottom right',
+                    })
+                })
+            }
+            else
+            {
+                console.log("Cannot Connect!");
+            }
 
-        socket.on('disconnect', function () {
-            console.log('disconnected');
+            socket.on('disconnect', function () {
+                console.log('Server Disconnected.');
+            });
         });
-    });
     </script>
 </body>
 </html>
